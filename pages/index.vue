21b1812b3f639f8ff77f4189cacd838a85be45c5
ss-DV8 <0sx3v4027253r1u@gmail.com>
2019-09-05 11:04:33 +0900

    TOPメニューからログアウトをしてもログイン情報が消えない

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
       <menu-button to="balance">残高照会</menu-button>
       <menu-button to="/statements">入出金明細</menu-button>
       <menu-button to="/transfer/bank">振込</menu-button>
+      <menu-button to="admin/banks">管理画面</menu-button>
+      <v-btn color="#9ACD32" block @click="logout">ログアウト</v-btn>
     </div>
     <div class="column"></div>
   </div>
@@ -16,5 +18,13 @@ import menuButton from "~/components/ui/menu-button";
 export default {
   components: { menuButton },
   middleware: "login",
+  methods: {
+    logout() {
+      this.$store.dispatch("login/id", null)
+      this.$router.push("/login")
+    }
+  }
 };
+
+
 </script>
